e72276eed0294eae9c2919c5f68557065d2a21e6
christine frank and joel stockamp <joel@>
2019-08-19 11:12:25 -0700

    add js code to make list of favorites appear

--- js/script.js
+++ js/script.js
@@ -1,3 +1,26 @@
-$(document).ready(function(event) {
-  
+$(document).ready(function() {
+  $("form").submit(function(event) {
+
+    $(".display ul").text("");
+
+    var favorites = [
+      $("#shape").val(),
+      $("#colour").val(),
+      $("#water").val(),
+      $("#landmass").val(),
+      $("#dinosaur").val()
+    ];
+    favorites.forEach(function(favItem) {
+      $(".display ul").append("<li>"+favItem+"</li>");
+    });
+
+    var truncate = [];
+    truncate.push(favorites[0], favorites[2]);
+
+    truncate.forEach(function(favItem) {
+      $(".display2 ul").append("<li>"+favItem+"</li>");
+    });
+
+    event.preventDefault();
+  });
 });
